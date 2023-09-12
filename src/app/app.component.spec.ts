35b97c6900ba98032eb789c4c4c1632dea1a6438
mrinali20 <wadbudemrinali@gmail.com>
2023-09-12 20:43:43 +0530

    angular

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'AST-course_Angular_session'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('AST-course_Angular_session');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AST-course_Angular_session app is running!');
+  });
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core'; //impot section
+
+@Component({  //component directive/decorator
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent { //class to write code here
+  title = 'My first Application';
+//let/var/const city = 'pune';
+//any Data type: city:any 
+//property binding :
+ city? : string ; //declaration of property
+
+ city2 : string = 'pune'; //declration + def of proprty
+
+ city3 = 'mumbai.....';
+ name='pooja';
+ no1 = 10;
+ no2= 200;
+ data ="aarav";
+ cars :any[] =['audi',"swift",'bmw'];
+ car =this.cars[1];
+ student ={name:'pooja',age :20};
+
+
+
+
+  constructor(){}    //to inject dependancies
+  
+//life cycle hooks 
+
+//functions
+// data binding : it is a technique to communicate b/w a component and the dom
+// 1.one way data binding 2.two way data binding
+//1.one way data binding :1.string interpolation 2.property binding 3.event binding
+// string interpolation :one way binding technique : .ts to dom( Document Object Model)/html/template{{}}
+
+//string interpolation{{}}: one way data binding technique: .ts to dom/html/template
+
+learningInterpolation(){ 
+  // void value return if donot return value.
+  return this.city2;
+  
+  
+  
+}
+// property blinding : [] define
+
+
+
+}
+
+  
